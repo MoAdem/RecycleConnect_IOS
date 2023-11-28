01b94e92d5762bd73654f77db9aceb42ba735e56
MoAdem <mohamedadem.torkhani@esprit.tn>
2023-11-28 15:46:33 +0100

    init0

---
+++ RecycleConnect_IOS/RecycleConnect_IOSApp.swift
@@ -0,0 +1,20 @@
+//
+//  RecycleConnect_IOSApp.swift
+//  RecycleConnect_IOS
+//
+//  Created by Med adem Torkhani   on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecycleConnect_IOSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
