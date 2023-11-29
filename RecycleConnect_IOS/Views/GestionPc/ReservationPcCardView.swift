f3b4862ff77e1620c519156c7cb48b7138568943
karimk <karimk@MM10L01.local>
2023-11-29 21:54:06 +0100

    add

--- RecycleConnect_IOS/Views/GestionPc/ReservationPcCardView.swift
+++ RecycleConnect_IOS/Views/GestionPc/ReservationPcCardView.swift
@@ -4,60 +4,7 @@
 //
 //  Created by karimk on 28/11/2023.
 //
-/*
-import SwiftUI
 
-struct ReservationPcCardView: View {
-    let reservationPc: ReservationPc
-
-    var body: some View {
-        HStack(spacing: 7) {
-            VStack(alignment: .leading, spacing: 9) {
-                Text("Votre commande a été ajoutée dans cette Point Collecte  ")
-                    .font(.system(size: 15))
-                    .fontWeight(.medium)
-                    .frame(maxWidth: .infinity, alignment: .center)
-
-                Text(reservationPc.Nom_R)
-                    .font(.body)
-                    .foregroundColor(.secondary)
-                    .frame(maxWidth: .infinity, alignment: .center)
-
-                Text(reservationPc.id_Pc)
-                    .font(.body)
-                    .foregroundColor(.secondary)
-                    .frame(maxWidth: .infinity, alignment: .center)
-
-                HStack {
-                    Button(action: {
-                        // Action for Supprimer
-                    }) {
-                        Text("Supprimer")
-                            .font(.system(size: 20))
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 50)
-                            .background(Color.green)
-                            .cornerRadius(50)
-                    }
-                    .padding(.leading, 20)
-                }
-            }
-            .padding(16)
-        }
-        .background(Color.white)
-        .cornerRadius(8)
-        .shadow(radius: 4)
-        .padding(8)
-    }
-}
-
-struct ReservationPcCardView_Previews: PreviewProvider {
-    static var previews: some View {
-        ReservationPcCardView(reservationPc: sampleReservationPcs[0])
-    }
-}
-*/
 import SwiftUI
 
 struct ReservationPcCardView: View {
