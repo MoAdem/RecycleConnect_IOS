77d3f55ddbf1aaaa5cb672f219b9fc4f0dbfb537
Aziz8888 <aziz.salem@esprit.tn>
2023-11-29 16:48:58 +0100

    update consommation delete et interface

--- RecycleConnect_IOS/RecycleConnect_IOSApp.swift
+++ RecycleConnect_IOS/RecycleConnect_IOSApp.swift
@@ -13,7 +13,7 @@ struct RecycleConnect_IOSApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ReservationView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
