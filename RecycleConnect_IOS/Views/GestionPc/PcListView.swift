95a3b07d8e731b78ddb407dd77840c536228f2df
karimk <karimk@MM10L01.local>
2023-11-29 21:53:11 +0100

    add PC

--- RecycleConnect_IOS/Views/GestionPc/PcListView.swift
+++ RecycleConnect_IOS/Views/GestionPc/PcListView.swift
@@ -4,71 +4,7 @@
 //
 //  Created by karimk on 28/11/2023.
 //
-/*
-import SwiftUI
 
-struct PcListView: View {
-    let pcs: [PC] = samplePCs
-
-    var body: some View {
-        NavigationView {
-            List(pcs) { pc in
-                PcCardView(pc: pc)
-                    .listRowInsets(EdgeInsets(top: 5, leading: 5, bottom: 4, trailing: 4))
-            }
-            .background(Image("background_splash_screen")
-                            .resizable()
-                            .scaledToFill()
-                            .edgesIgnoringSafeArea(.all))
-            .navigationTitle("Points de Collecte")
-            .navigationBarTitleDisplayMode(.inline)
-        }
-    }
-}
-
-struct PcListView_Previews: PreviewProvider {
-    static var previews: some View {
-        PcListView()
-    }
-}
-*/
-/*
-import SwiftUI
-
-struct PcListView: View {
-    let pcs: [PC] = samplePCs
-    @State private var isReservationActive: Bool = false
-
-    var body: some View {
-        NavigationView {
-            List(pcs) { pc in
-                NavigationLink(
-                    destination: ReservationPcListView(),
-                    isActive: $isReservationActive,
-                    label: {
-                        PcCardView(pc: pc)
-                            .listRowInsets(EdgeInsets(top: 5, leading: 5, bottom: 4, trailing: 4))
-                    }
-                )
-            }
-            .background(
-                Image("background_splash_screen")
-                    .resizable()
-                    .scaledToFill()
-                    .edgesIgnoringSafeArea(.all)
-            )
-            .navigationTitle("Points de Collecte")
-            .navigationBarTitleDisplayMode(.inline)
-        }
-    }
-}
-
-struct PcListView_Previews: PreviewProvider {
-    static var previews: some View {
-        PcListView()
-    }
-}
-*/
 import SwiftUI
 
 struct PcListView: View {
