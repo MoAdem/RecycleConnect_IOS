b76193b45dbb61c5d1de2c2c815e6a327f00f52d
karimk <karimk@MM10L01.local>
2023-11-29 21:58:03 +0100

    add Det

--- RecycleConnect_IOS/Views/GestionPc/NavView.swift
+++ RecycleConnect_IOS/Views/GestionPc/NavView.swift
@@ -23,7 +23,7 @@ struct NavView: View {
                     Text("Go to Mapp")
                 }
             }
-           // .navigationTitle("Navigation Example")
+           
         }
     }
 }
