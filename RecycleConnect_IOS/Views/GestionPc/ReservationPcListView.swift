7692233369bd5aeaf1164b2743a30232d6cb5a09
karimk <karimk@MM10L01.local>
2023-11-29 21:54:42 +0100

    add Res

--- RecycleConnect_IOS/Views/GestionPc/ReservationPcListView.swift
+++ RecycleConnect_IOS/Views/GestionPc/ReservationPcListView.swift
@@ -4,64 +4,7 @@
 //
 //  Created by karimk on 28/11/2023.
 //
-/*
-import SwiftUI
 
-struct ReservationPcListView: View {
-    let reservationPcs: [ReservationPc] = sampleReservationPcs
-
-    var body: some View {
-        NavigationView {
-            List(reservationPcs) { reservationPc in
-                ReservationPcCardView(reservationPc: reservationPc)
-                    .listRowInsets(EdgeInsets(top: 5, leading: 5, bottom: 4, trailing: 4))
-            }
-            .background(Image("background_splash_screen")
-                            .resizable()
-                            .scaledToFill()
-                            .edgesIgnoringSafeArea(.all))
-            .navigationTitle("Reservation PC").navigationBarTitleDisplayMode(.inline)
-        }
-    }
-}
-
-struct ReservationPcListView_Previews: PreviewProvider {
-    static var previews: some View {
-        ReservationPcListView()
-    }
-}
- */
-/*
-import SwiftUI
-
-struct ReservationPcListView: View {
-    let reservationPcs: [ReservationPc] = sampleReservationPcs
-
-    var body: some View {
-        NavigationView {
-            List(reservationPcs) { reservationPc in
-                ReservationPcCardView(reservationPc: reservationPc)
-                    .listRowInsets(EdgeInsets(top: 5, leading: 5, bottom: 4, trailing: 4))
-            }
-            .background(
-                Image("background_splash_screen")
-                    .resizable()
-                    .scaledToFill()
-                    .edgesIgnoringSafeArea(.all)
-            )
-            .navigationTitle("Reservation PC")
-            .navigationBarTitleDisplayMode(.inline)
-        }
-    }
-}
-
-struct ReservationPcListView_Previews: PreviewProvider {
-    static var previews: some View {
-        ReservationPcListView()
-    }
-}
-
-*/
 import SwiftUI
 
 struct ReservationPcListView: View {
