2b785feda67ba5df670ca98d8ea68d2a9165726d
karimk <karimk@MM10L01.local>
2023-12-07 00:53:21 +0100

    affiche maps

--- RecycleConnect_IOS/Views/GestionPc/MappView.swift
+++ RecycleConnect_IOS/Views/GestionPc/MappView.swift
@@ -4,6 +4,30 @@
 //
 //  Created by abir on 29/11/2023.
 //
+import SwiftUI
+import MapKit
+
+struct AnnotationItem: Identifiable {
+    var id = UUID()
+    var annotation: MKPointAnnotation
+}
+
+struct MappView: View {
+    @State private var mapRegion: MKCoordinateRegion
+    let annotations: [AnnotationItem]
+
+    init(mapRegion: MKCoordinateRegion, annotations: [AnnotationItem]) {
+        self._mapRegion = State(initialValue: mapRegion)
+        self.annotations = annotations
+    }
+
+    var body: some View {
+        Map(coordinateRegion: $mapRegion, showsUserLocation: true, annotationItems: annotations) { annotationItem in
+            MapPin(coordinate: annotationItem.annotation.coordinate, tint: .red)
+        }
+    }
+}
+
 
 /*
 import SwiftUI
@@ -54,63 +78,5 @@ struct MappView_Previews: PreviewProvider {
         MappView()
     }
 }
-*//*
-import SwiftUI
-import MapKit
-
-struct AnnotationItem: Identifiable {
-    var id = UUID()
-    var annotation: MKPointAnnotation
-}
-
-struct MappView: View {
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 36.809328, longitude: 10.086327), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
-
-    @State private var annotations = [
-        AnnotationItem(annotation: {
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = CLLocationCoordinate2D(latitude: 36.809328, longitude: 10.086327)
-            annotation.title = "Manouba"
-            annotation.subtitle = "Manouba"
-            return annotation
-        }())
-        // Add more annotations as needed
-    ]
-
-    var body: some View {
-        Map(coordinateRegion: $mapRegion, showsUserLocation: true, annotationItems: annotations) { annotationItem in
-            MapPin(coordinate: annotationItem.annotation.coordinate, tint: .red)
-        }
-    }
-}
-
-struct MappView_Previews: PreviewProvider {
-    static var previews: some View {
-        MappView()
-    }
-}
 */
-import SwiftUI
-import MapKit
-
-struct AnnotationItem: Identifiable {
-    var id = UUID()
-    var annotation: MKPointAnnotation
-}
-
-struct MappView: View {
-    @State private var mapRegion: MKCoordinateRegion
-    let annotations: [AnnotationItem]
-
-    init(mapRegion: MKCoordinateRegion, annotations: [AnnotationItem]) {
-        self._mapRegion = State(initialValue: mapRegion)
-        self.annotations = annotations
-    }
-
-    var body: some View {
-        Map(coordinateRegion: $mapRegion, showsUserLocation: true, annotationItems: annotations) { annotationItem in
-            MapPin(coordinate: annotationItem.annotation.coordinate, tint: .red)
-        }
-    }
-}
 
