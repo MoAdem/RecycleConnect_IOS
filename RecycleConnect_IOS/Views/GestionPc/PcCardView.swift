f63c2bec02a764641774e1acaf2bb500fe26acfd
karimk <karimk@MM10L01.local>
2023-12-12 07:25:51 +0100

    PC

--- RecycleConnect_IOS/Views/GestionPc/PcCardView.swift
+++ RecycleConnect_IOS/Views/GestionPc/PcCardView.swift
@@ -14,73 +14,74 @@ struct PcCardView: View {
     @ObservedObject var viewModel: ReservationPcViewModel
 
     var body: some View {
-        VStack {
-            HStack(spacing: 7) {
+        VStack  {
+            HStack {
                 Button(action: {
                     // Actions à effectuer lors du clic sur l'image
                     isMapViewActive.toggle()
                 }) {
-                    Image("ImagePos")
+                    Image("po")
                         .resizable()
                         .aspectRatio(contentMode: .fill)
-                        .frame(width: 150, height: 150)
+                        .frame(width: 70, height: 70)
+                        .cornerRadius(10)
+                        .offset(x: 4)
                         .clipped()
                 }
 
-                VStack(alignment: .leading, spacing: 9) {
+                VStack(alignment: .leading, spacing: 5) {
                     Text(pc.Nom_Pc)
                         .font(.system(size: 18))
                         .fontWeight(.medium)
-
-                    HStack {
-                        Spacer()
-                        Text(pc.address_Pc)
-                            .font(.system(size: 12))
-                            .foregroundColor(.black)
-                        Spacer()
-                    }
-
-                    HStack {
-                        Spacer()
-                        Text(pc.address_mail_Pc)
-                            .font(.system(size: 9))
-                            .foregroundColor(.black)
-                        Spacer()
-                    }
-
-                    HStack {
-                        Spacer()
-                        Text(String(pc.numero_tel))
-                            .font(.system(size: 12))
-                            .foregroundColor(.black)
-                        Spacer()
-                    }
-
-                    Button(action: {
-                        // Actions à effectuer lors du clic sur le bouton "Sélectionner"
-                        viewModel.createReservation(nomR: pc.Nom_Pc, idPc: pc.id)
-                        //viewModel.createReservation(nomR: "votre_Article", idPc: pc.id)
-                        isReservationActive.toggle()
-                    }) {
-                        Text("Sélectionner")
-                            .font(.system(size: 20))
-                            .foregroundColor(.white)
-                            .frame(width: 150, height: 50)
-                            .background(Color(red: 0.05, green: 0.54, blue: 0.48))
-                            .cornerRadius(50)
-                    }
-                    .background(
-                        NavigationLink(
-                            destination: ReservationPcListView(viewModel: viewModel),
-                            isActive: $isReservationActive,
-                            label: {
-                                EmptyView()
-                            })
+                    
+                    
+                    Text(pc.address_Pc)
+                        .font(.system(size: 12))
+                        .foregroundColor(.black)
+                    
+                    Text(pc.address_mail_Pc)
+                        .font(.system(size: 12))
+                        .foregroundColor(.black)
+                    
+                    
+                    Text(String(pc.numero_tel))
+                        .font(.system(size: 12))
+                        .foregroundColor(.black)
+                }.padding(20)
+                
+                Spacer()
+            
+                    HStack{
+                        Button(action: {
+                            // Actions à effectuer lors du clic sur le bouton "Sélectionner"
+                            //viewModel.createReservation(nomR: pc.Nom_Pc, idPc: pc.id)
+                            viewModel.createReservation(nomR: "votre_Article", idPc: pc.id)
+                            isReservationActive.toggle()
+                        }) {
+                            Text("Sélectionner")
+                                .font(.system(size: 11, weight: .bold))
+                                .foregroundColor(.white)
+                                .frame(width: 76, height: 33)
+                                .background(Color(red: 0.05, green: 0.54, blue: 0.48))
+                                .cornerRadius(10)
+                                .offset(x: 4 , y: 37)
+                            
+                        }
+                        .background(
+                            NavigationLink(
+                                destination: ReservationPcListView(viewModel: viewModel),
+                                isActive: $isReservationActive,
+                                label: {
+                                    EmptyView()
+                                })
                             .hidden()
-                    )
-                }
-                .padding(16)
+                        )
+                    }
+               
+                
+                Spacer()
             }
+            .frame(maxWidth: .infinity)
             .background(Color.white)
             .cornerRadius(8)
             .overlay(
@@ -88,7 +89,7 @@ struct PcCardView: View {
                     .stroke(Color(red: 0.05, green: 0.54, blue: 0.48), lineWidth: 2)
             )
             .shadow(radius: 4)
-            .padding(8)
+            .padding(5)
         }
         .background(
             NavigationLink(
